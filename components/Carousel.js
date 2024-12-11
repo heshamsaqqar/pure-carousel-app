c4e391ee80bf56ef0c0a3e9d67b6a726816078da
Hesham Saqqar <saqqar977@gmail.com>
2024-12-11 11:45:44 +0000

    minimal

---
+++ components/Carousel.js
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react'
+import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+function Carousel() {
+  return (
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={125}
+      totalSlides={3}
+    >
+      <Slider>
+        <Slide index={0}>I am the first Slide.</Slide>
+        <Slide index={1}>I am the second Slide.</Slide>
+        <Slide index={2}>I am the third Slide.</Slide>
+      </Slider>
+    </CarouselProvider>
+  )
+}
+
+export default Carousel
